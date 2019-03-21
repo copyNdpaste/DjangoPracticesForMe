93cc997fc65381bea393453c09712e71d19e52ac
copyndpaste <julsnutini@gmail.com>
2019-03-21 14:08:55 +0900

    [For Django Newbie] 하나의 함수에서 GET, POST 방식 분기해서 데이터 처리 2019.03.21

--- workspace/myweb/blog/views.py
+++ workspace/myweb/blog/views.py
@@ -13,9 +13,13 @@ def write(request) :
 def syntax(request):
     return render (request, 'blog/syntax.html')'''
 
-def title(request):
-    data = request.GET.get('name') #GET 방식으로 요청 받은 URL. 여러 파라미터 중 어떤 걸 가져올지 파라미터 명시.
-    ver = request.GET.get('ver')
+def title(request): #하나의 함수에서 GET, POST 요청 분기
+    if request.method == 'GET':
+        data = request.GET.get('name') #GET 방식으로 요청 받은 URL. 여러 파라미터 중 어떤 걸 가져올지 파라미터 명시.
+        ver = request.GET.get('ver')
+    elif request.method == 'POST':
+        data = request.POST.get('name')
+        ver = request.POST.get('ver')
     context = {
         'data': data,
         'version' : ver
