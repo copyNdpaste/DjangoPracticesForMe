136704d60552e60970510037ab097048aa8c3b12
copyndpaste <julsnutini@gmail.com>
2019-03-22 14:56:11 +0900

    [For Django Newbie] 테이블명.objects.함수() 함수 all(), get(), filter(), exclude() 조건 __gt 초과, __lt 미만, __gte 이상, __lte 이하, __range 범위, __contains 문자열 포함 2019.03.22

---
+++ workspace/myweb/myacc/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+# 테이블 이름 : AccountBook(가계부)
+# 컬럼 정보
+#     항목      : 문자열(100), 빈 문자열(blank) 허용
+#     수입 금액 : 정수, 기본 값 0
+#     지출 금액 : 정수, 기본 값 0
+#     수량      : 정수, 기본 값 0
+#     날짜      : 날짜
+
+class AccountBook(models.Model):
+    item = models.CharField(max_length=100, blank=True)
+    inc = models.IntegerField(default=0)
+    outc = models.IntegerField(default=0)
+    count = models.IntegerField(default=0)
+    date = models.DateField()
--- workspace/myweb/myweb/settings.py
+++ workspace/myweb/myweb/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myweb', 'htmlapp', 'blog',
+    'myweb', 'htmlapp', 'blog', 'myacc',
 ]
 
 MIDDLEWARE = [
