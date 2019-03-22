def68a655b854cc173d6fb641630f22876c852e1
copyndpaste <julsnutini@gmail.com>
2019-03-22 15:21:07 +0900

    [For Django Newbie] 테이블명.objects.함수() 2019.03.22

--- workspace/myweb/myacc/models.py
+++ workspace/myweb/myacc/models.py
@@ -9,6 +9,10 @@ from django.db import models
 #     수량      : 정수, 기본 값 0
 #     날짜      : 날짜
 
+'''테이블명.objects.values : 사전
+테이블명.objects.values_list : 튜플
+테이블명.objects.order_by : 정렬 ('-inc', 'outc' ) 역순'''
+
 class AccountBook(models.Model):
     item = models.CharField(max_length=100, blank=True)
     inc = models.IntegerField(default=0)
