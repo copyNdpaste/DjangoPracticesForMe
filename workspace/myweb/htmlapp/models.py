4e9c815dc4dbc0f79c34b1be1eaaa4cf62f396a6
copyndpaste <julsnutini@gmail.com>
2019-03-22 13:16:03 +0900

    [For Django Newbie] Model 사용하기. 문자열 models.CharField() (max_length 지정해야 함), models.TextField() (데이터 제한 없이 저장) 정수 models.IntegerField() (32비트), models.SmallIntegerField() (16비트), models.BigIntegerField() (64비트) 실수 models.DecimalField() (고정 소수점, max_digits=int, decimal_places=int), models.FloatField() (부동 소수점) 부울 models.BooleanField() 날짜 models.DateField(), models.TimeField(), models.DateTimeField()          날짜 옵션 : auto_now=True|False (현재 날짜 또는 시간 자동 저장, EX) 수정일), auto_now_add=True|False (데이터 처음 생성 시 한번만 현재 날짜 또는 시간 자동 저장 EX) 생성일) 2019.03.22

--- workspace/myweb/htmlapp/models.py
+++ workspace/myweb/htmlapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TableName(models.Model):
+    id = models.AutoField() # 데이터 자동 증가
+    name = models.CharField(max_length=50)
+    age = models.IntegerField(default=1)
+    phone = models.CharField(null=False, blank=False)
+    home_addr = models.CharField(null=True, blank=True)
+    create_date = models.DateField()
