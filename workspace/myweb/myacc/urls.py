d2a3964d5a789657522e7eb6f0a0e1540f577b0c
copyndpaste <julsnutini@gmail.com>
2019-03-25 14:14:05 +0900

    [For Django Newbie] location.href 써서 이동 2019.03.25

--- workspace/myweb/myacc/urls.py
+++ workspace/myweb/myacc/urls.py
@@ -3,4 +3,6 @@ from myacc import views
 
 urlpatterns = [
     path('', views.index),
+    #path('printing/', views.printing),
+    path('<str:acc_type>/', views.index),
 ]
--- workspace/myweb/myacc/views.py
+++ workspace/myweb/myacc/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from myacc.models import AccountBook
+from django.shortcuts import HttpResponse
 
 # Create your views here.
-def index(request):
+def index(request, acc_type=None):
     datas = AccountBook.objects.all()
+    if acc_type == 'in':
+        datas = datas.filter(inc__gt=0)
+    else:
+        datas = datas.filter(outc__gt=0)
     context = {
         'datas': datas
     }
     return render(request, 'myacc/index.html', context)
+'''
+def accTypeSearch(request, acc_type):
+    if acc_type == 'in':
+        datas = AccountBook.objects.filter(inc__gt=0)
+    elif acc_type == 'out':
+        datas = AccountBook.objects.filter(outc__gt=0)
+    else:
+        return HttpResponse('잘못된 요청')
+    context = {
+        'datas': datas
+    }
+    return render(request, 'myacc/index.html', context)
+'''
